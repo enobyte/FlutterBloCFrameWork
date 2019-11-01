b83bb921c3ffc0c237311623262071298c542899
enobyte <enoraden@gmail.com>
2019-11-01 16:37:53 +0700

    bloc

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Eno/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/eno/Downloads/BaseFramework"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Eno/flutter/bin/cache/artifacts/engine/ios"
